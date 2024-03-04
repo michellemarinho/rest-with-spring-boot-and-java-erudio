cfb4d26920bb3353979ee305d5ee868bd587b60e
Michelle <michellemmelo@yahoo.com.br>
2024-03-03 21:12:48 -0300

    First steps in Java with Spring Boot 3

---
+++ 10_VersionamentoEndPoints/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -0,0 +1,150 @@
+package br.com.erudio.controllers;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.erudio.data.vo.v1.PersonVO;
+import br.com.erudio.data.vo.v2.PersonVOV2;
+import br.com.erudio.services.PersonServices;
+
+//contém vários métodos manipuladores
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+	
+	@Autowired //Com a inclusão do Autowired, o spring boot vai cuidar da instanciação de forma dinamica em tempo de execução
+	private PersonServices service; //injeção de dependencias
+	//private PersonServices service = new PersonServices();
+	
+	//Pode-se usar o browser para acessar o GET, mas o POST, PUT e DELETE só podem ser acessados pelo Postman. 
+	
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<PersonVO> findAll () {
+			return service.findAll();
+		}
+	
+	//"http://localhost:8080/person/1" me devolve os dados do boneco q tá descrito na classe "PersonServices"
+	@GetMapping(value = "/{id}",
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public PersonVO findById (@PathVariable(value = "id")Long id) {
+			return service.findById(id);
+		}
+	
+	
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public PersonVO create (@RequestBody PersonVO person) {
+			return service.create(person); //cria
+		}
+	//Foi incluído esse novo PostMapping por conta da inclusão do campo Biday
+	@PostMapping(value = "/v2", consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public PersonVOV2 createV2 (@RequestBody PersonVOV2 person) {
+			return service.createV2(person); //cria
+		}
+	
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public PersonVO update (@RequestBody PersonVO person) {
+				return service.update(person); //atualiza
+		}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete (@PathVariable(value = "id")Long id) {
+				service.delete(id); //apaga
+				return ResponseEntity.noContent().build(); //para retornar o status code de erro, correto
+		}
+}
+
+//	@RequestMapping(value = "/subtraction/{numberOne}/{numberTwo}",method=RequestMethod.GET)
+//
+//	public Double subtraction ( 
+//			@PathVariable(value = "numberOne")String numberOne,
+//			@PathVariable(value = "numberTwo")String numberTwo
+//	)throws Exception {
+//		
+//		if(!NumberConverter.isNumeric(numberOne)|| !NumberConverter.isNumeric(numberTwo)) { 
+//			throw new UnsupportedMathOperationException("Please set a numeric value!");
+//		}
+//		
+//		return math.subtraction(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
+//		}
+//	
+//	@RequestMapping(value = "/multiplication/{numberOne}/{numberTwo}",method=RequestMethod.GET)
+//
+//	public Double multiplication ( 
+//			@PathVariable(value = "numberOne")String numberOne,
+//			@PathVariable(value = "numberTwo")String numberTwo
+//			//@PathVariable para retornar dados da url
+//	)throws Exception {
+//		
+//		if(!NumberConverter.isNumeric(numberOne)|| !NumberConverter.isNumeric(numberTwo)) { 
+//			throw new UnsupportedMathOperationException("Please set a numeric value!");
+//		}
+//		
+//		return math.multiplication(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
+//		}
+//	
+//	@RequestMapping(value = "/division/{numberOne}/{numberTwo}",method=RequestMethod.GET)
+//
+//	public Double division ( 
+//			@PathVariable(value = "numberOne")String numberOne,
+//			@PathVariable(value = "numberTwo")String numberTwo
+//	)throws Exception {
+//		
+//		if(!NumberConverter.isNumeric(numberOne)|| !NumberConverter.isNumeric(numberTwo)) { 
+//			throw new UnsupportedMathOperationException("Please set a numeric value!");
+//		}
+//		
+//		return math.division(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
+//		}
+//
+//
+//		@RequestMapping(value = "/mean/{numberOne}/{numberTwo}",method=RequestMethod.GET)
+//
+//		public Double mean ( 
+//				@PathVariable(value = "numberOne")String numberOne,
+//				@PathVariable(value = "numberTwo")String numberTwo
+//		)throws Exception {
+//			
+//			if(!NumberConverter.isNumeric(numberOne)|| !NumberConverter.isNumeric(numberTwo)) { 
+//				throw new UnsupportedMathOperationException("Please set a numeric value!");
+//			}
+//			
+//		return math.mean(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
+//			}
+//	
+//		@RequestMapping(value = "/squareroot/{number}",method=RequestMethod.GET)
+//
+//		public Double squareroot ( 
+//				@PathVariable(value = "number")String number
+//		)throws Exception {
+//			
+//			if(!NumberConverter.isNumeric(number)) { 
+//				throw new UnsupportedMathOperationException("Please set a numeric value!");
+//			}
+//			
+//			return math.squareroot(NumberConverter.convertToDouble(number));
+//			}
+	
+	
+//	@RequestMapping("/greeting")
+//	public Double greeting (@RequestParam(value = "name", defaultValue = "World")String name) {
+//		return new Greeting(0, String.format(template, name));
+//	}
+	
+	//O @RequestMapping mapeia uma requisição para um método e torna isso um endereço http para acessar via browser.
+	//No browser http://localhost:8080/greeting
+	//No broser http://localhost:8080/greeting?name=Michelle
+	//CTRL+SHIFT+O organiza os imports
+
