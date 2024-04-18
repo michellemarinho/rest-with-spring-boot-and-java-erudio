a3aec335959a0d56e48c201f66991c5388189081
Michelle <michellemmelo@yahoo.com.br>
2024-04-18 20:12:44 -0300

    my backup 24042024

---
+++ 17_AutenticaçãoComJWT151_2/rest-with-spring-boot-and-java-michelle/src/main/java/br/com/erudio/exceptions/RequiredObjectIsNullException.java
@@ -0,0 +1,22 @@
+package br.com.erudio.exceptions;
+//Classe para criar as próprias exceções
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+//Sempre que ocorre uma "UnsupportedMathOperationException" será retornada uma @ResponseStatus que é um código de erro
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class RequiredObjectIsNullException  extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;	
+	
+	
+	public RequiredObjectIsNullException() {
+		super("It is not allowed to persist a null object!");
+	}
+	
+	public RequiredObjectIsNullException(String ex) {
+		super(ex);
+	}
+	
+}
---
+++ 17_AutenticaçãoComJWT151_2/rest-with-spring-boot-and-java-michelle/target_/classes/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Maceió', 'Michelle', 'Female', 'Marinho'),
+	(2, 'Anchiano  - Italy', 'Leonardo', 'Male', 'Da Vinci'),
+	(4, 'Nikity-Brasil', 'Rafael', 'Male', 'Marinho'),
+	(5, 'Nikity-RJ-Brasil', 'Gustavo', 'Male', 'Marinho'),
+	(7, 'India', 'TajMahal', 'Male', 'Gandhi'),
+	(11, 'Maceió - AL -21', 'Mimimi', 'Female', 'Aranda');
