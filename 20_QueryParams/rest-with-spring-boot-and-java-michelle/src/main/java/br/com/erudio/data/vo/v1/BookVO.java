900e83636c4434b8429251790b708143178432e7
Michelle <michellemmelo@yahoo.com.br>
2024-05-05 16:56:19 -0300

    my backup 05052024

---
+++ 20_QueryParams/rest-with-spring-boot-and-java-michelle/src/main/java/br/com/erudio/data/vo/v1/BookVO.java
@@ -0,0 +1,120 @@
+package br.com.erudio.data.vo.v1;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.springframework.hateoas.RepresentationModel;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.github.dozermapper.core.Mapping;
+
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@JsonPropertyOrder({"id", "author", "launchDate", "price", "title"}) //ordenar
+public class BookVO extends RepresentationModel<BookVO> implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@JsonProperty ("id") //anotation para exibir a palavra id ao invés de Key no retorno no postman
+	@Mapping ("id") //anotation para q a aplicação reconheça que id é uma chamada para a chave Key
+	private Long key;
+	private String author;
+	private Date launchDate; //java.util
+	private Double price;
+	private String title;
+	
+	public BookVO() {}
+
+	//Seleciona a opção Source/Generate Getters and Setters (Obs: select all, menos a opção SerialVersionUID)
+	
+	public Long getKey() {
+		return key;
+	}
+
+	public void setKey(Long key) {
+		this.key = key;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Date getLaunchDate() {
+		return launchDate;
+	}
+
+	public void setLaunchDate(Date launchDate) {
+		this.launchDate = launchDate;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	//Seleciona a opção Source/Generate hashCode() and equals()...
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((author == null) ? 0 : author.hashCode());
+		result = prime * result + ((key == null) ? 0 : key.hashCode());
+		result = prime * result + ((launchDate == null) ? 0 : launchDate.hashCode());
+		result = prime * result + ((price == null) ? 0 : price.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BookVO other = (BookVO) obj;
+		if (author == null) {
+			if (other.author != null)
+				return false;
+		} else if (!author.equals(other.author))
+			return false;
+		if (key == null) {
+			if (other.key != null)
+				return false;
+		} else if (!key.equals(other.key))
+			return false;
+		if (launchDate == null) {
+			if (other.launchDate != null)
+				return false;
+		} else if (!launchDate.equals(other.launchDate))
+			return false;
+		if (price == null) {
+			if (other.price != null)
+				return false;
+		} else if (!price.equals(other.price))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}	
+}
